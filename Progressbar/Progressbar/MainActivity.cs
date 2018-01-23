301e2d2234923be2002a27b2ec742eccfbdb7279
admin_pk\Admin <Cojucovschi@gmail.com>
2018-01-23 16:57:06 +0200

    First Commit

---
+++ Progressbar/Progressbar/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Progressbar
+{
+    [Activity(Label = "Progressbar", MainLauncher = true, Theme = "@style/AppTheme")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
